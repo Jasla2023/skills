0805a92a197190c5efd96844275bb9c558e323f1
Laurin <l.sivabalan@outlook.de>
2023-03-29 16:03:36 +0200

    Create Project "Skills"

---
+++ Skills/employees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skills
+{
+    class employees
+    {
+        int employee_Id;
+        string FirstName;
+        string LastName;
+    }
+}
