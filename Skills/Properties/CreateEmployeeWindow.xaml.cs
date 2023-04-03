7a038a96683ff56bd130a9fe3284db75b643443b
Lukas <lukasmz91@gmail.com>
2023-04-03 10:51:19 +0200

    added the ability to add additional skills, added behavior

--- Skills/Properties/CreateEmployeeWindow.xaml.cs
+++ Skills/Properties/CreateEmployeeWindow.xaml.cs
@@ -49,10 +49,14 @@ namespace Skills.Properties
         private List<Label> addedSkillLevelLabels = new List<Label>();
         private List<ComboBox> addedSkillLevelComboBoxes = new List<ComboBox>();
 
-        private void Button_Click1(object sender, RoutedEventArgs e)
+        private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-
+            Close();
         }
+
+       
+
+
 
         /// <summary>
         /// 
@@ -60,120 +64,138 @@ namespace Skills.Properties
         /// <param name="sender"></param>
         /// <param name="e"></param>
 
-        //private void btnAddMoreSkills_Click(object sender, RoutedEventArgs e)
-        //{
-        //    CreateEmployee.Height += 30;
-        //    grd2createEmployee.RowDefinitions.Add(new RowDefinition());
+        private void btnAddMoreSkills_Click(object sender, RoutedEventArgs e)
+        {
+            Height += 70;
+            grdEntries.Height += 60;
+            
+            grdEntries.RowDefinitions.Add(new RowDefinition());
+            grdEntries.RowDefinitions.Add(new RowDefinition());
+            grdEntries.RowDefinitions.Add(new RowDefinition());
 
-        //    addedSkillLabels.Add(new Label());
-        //    addedSkillLabels.Last().Content = "Kenntniss/Erfahrung: ";
-        //    Grid.SetRow(addedSkillLabels.Last(), 1 + rowsAdded);
-        //    Grid.SetColumn(addedSkillLabels.Last(), 0);
-        //    addedSkillLabels.Last().HorizontalAlignment = HorizontalAlignment.Right;
-        //    addedSkillLabels.Last().VerticalAlignment = VerticalAlignment.Center;
+            addedSkillLabels.Add(new Label());
+            addedSkillLabels.Last().Content = "Kenntnisse: ";
+            Grid.SetRow(addedSkillLabels.Last(), 6 + rowsAdded);
+            Grid.SetColumn(addedSkillLabels.Last(), 0);
+            addedSkillLabels.Last().HorizontalAlignment = HorizontalAlignment.Left;
+            addedSkillLabels.Last().VerticalAlignment = VerticalAlignment.Center;
 
-        //    addedSkillTextBoxes.Add(new TextBox());
-        //    Grid.SetRow(addedSkillTextBoxes.Last(), 1 + rowsAdded);
-        //    Grid.SetColumn(addedSkillTextBoxes.Last(), 1);
-        //    addedSkillTextBoxes.Last().HorizontalAlignment = HorizontalAlignment.Left;
-        //    addedSkillTextBoxes.Last().VerticalAlignment= VerticalAlignment.Center;
-        //    addedSkillTextBoxes.Last().Width = 150;
+            addedSkillTextBoxes.Add(new TextBox());
+            Grid.SetRow(addedSkillTextBoxes.Last(), 6 + rowsAdded);
+            Grid.SetColumn(addedSkillTextBoxes.Last(), 1);
+            addedSkillTextBoxes.Last().HorizontalAlignment = HorizontalAlignment.Left;
+            addedSkillTextBoxes.Last().VerticalAlignment = VerticalAlignment.Center;
+            addedSkillTextBoxes.Last().Width = 150;
+            addedSkillTextBoxes.Last().Style = new Style();
+            addedSkillTextBoxes.Last().Height = 25;
+                
+            addedSkillLevelLabels.Add(new Label());
+            addedSkillLevelLabels.Last().Content = "Kentnissstufe: ";
+            Grid.SetRow(addedSkillLevelLabels.Last(), 7 + rowsAdded);
+            Grid.SetColumn(addedSkillLevelLabels.Last(), 0);
+            addedSkillLevelLabels.Last().HorizontalAlignment = HorizontalAlignment.Left;
+            addedSkillLevelLabels.Last().VerticalAlignment = VerticalAlignment.Center;
 
-        //    addedSkillLevelLabels.Add(new Label());
-        //    addedSkillLevelLabels.Last().Content = "Stufe: ";
-        //    Grid.SetRow(addedSkillLevelLabels.Last(), 1 + rowsAdded);
-        //    Grid.SetColumn(addedSkillLevelLabels.Last(), 2);
-        //    addedSkillLevelLabels.Last().HorizontalAlignment= HorizontalAlignment.Right;
-        //    addedSkillLevelLabels.Last().VerticalAlignment = VerticalAlignment.Center;
+            addedSkillLevelComboBoxes.Add(new ComboBox());
+            addedSkillLevelComboBoxes.Last().Items.Add(new ComboBoxItem { Content = "Grundkenntnisse", IsSelected = true });
+            addedSkillLevelComboBoxes.Last().Items.Add(new ComboBoxItem { Content = "Fortgeschrittene Kenntnisse" });
+            addedSkillLevelComboBoxes.Last().Items.Add(new ComboBoxItem { Content = "Bereits in Projekt eingesetzt" });
+            addedSkillLevelComboBoxes.Last().Items.Add(new ComboBoxItem { Content = "Umfangreiche Projekterfahrungen" });
+            Grid.SetRow(addedSkillLevelComboBoxes.Last(), 7 + rowsAdded);
+            Grid.SetColumn(addedSkillLevelComboBoxes.Last(), 1);
+            addedSkillLevelComboBoxes.Last().HorizontalAlignment = HorizontalAlignment.Left;
+            addedSkillLevelComboBoxes.Last().VerticalAlignment = VerticalAlignment.Center;
 
-        //    addedSkillLevelComboBoxes.Add(new ComboBox());
-        //    addedSkillLevelComboBoxes.Last().Items.Add(new ComboBoxItem { Content = "Grundkenntnisse", IsSelected=true});
-        //    addedSkillLevelComboBoxes.Last().Items.Add(new ComboBoxItem { Content = "Fortgeschrittene Kenntnisse" });
-        //    addedSkillLevelComboBoxes.Last().Items.Add(new ComboBoxItem { Content = "Bereits in Projekt eingesetzt" });
-        //    addedSkillLevelComboBoxes.Last().Items.Add(new ComboBoxItem { Content = "Umfangreiche Projekterfahrungen" });
-        //    Grid.SetRow(addedSkillLevelComboBoxes.Last(), 1 + rowsAdded);
-        //    Grid.SetColumn(addedSkillLevelComboBoxes.Last(), 3);
-        //    addedSkillLevelComboBoxes.Last().HorizontalAlignment = HorizontalAlignment.Left;
-        //    addedSkillLevelComboBoxes.Last().VerticalAlignment= VerticalAlignment.Center;
+            Grid.SetRow(btnAddMoreSkills, 8 + rowsAdded);
 
-        //    Grid.SetRow(btnAddMoreSkills, 2 + rowsAdded);
+            grdEntries.Children.Add(addedSkillLabels.Last());
+            grdEntries.Children.Add(addedSkillTextBoxes.Last());
+            grdEntries.Children.Add(addedSkillLevelLabels.Last());
+            grdEntries.Children.Add(addedSkillLevelComboBoxes.Last());
 
-        //    grd2createEmployee.Children.Add(addedSkillLabels.Last());
-        //    grd2createEmployee.Children.Add(addedSkillTextBoxes.Last());
-        //    grd2createEmployee.Children.Add(addedSkillLevelLabels.Last());
-        //    grd2createEmployee.Children.Add(addedSkillLevelComboBoxes.Last());
+            Grid.SetRow(btnClose, 7 + rowsAdded);
+            Grid.SetRow(btnSave, 7 + rowsAdded);
 
-        //    rowsAdded++;
+            rowsAdded+=2;
 
-        //}
+        }
 
-        //private int AssignSkillLevel(ComboBox skillLevel)
-        //{
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
 
-        //    int level;
-        //    switch ((skillLevel.SelectedItem as ComboBoxItem).Content.ToString())
-        //    {
-        //        case "Grundkenntnisse": level = 1;
-        //            break;
+        }
 
-        //        case "Fortgeschrittene Kenntnisse": level = 2;
-        //            break;
+        private int AssignSkillLevel(ComboBox skillLevel)
+        {
 
-        //        case "Bereits in Projekt eingesetzt": level = 3;
-        //            break;
+            int level;
+            switch ((skillLevel.SelectedItem as ComboBoxItem).Content.ToString())
+            {
+                case "Grundkenntnisse":
+                    level = 1;
+                    break;
 
+                case "Fortgeschrittene Kenntnisse":
+                    level = 2;
+                    break;
 
-        //        case "Umfangreiche Projekterfahrungen": level = 4;
-        //            break;
-
-
-        //        default: throw new ArgumentException("" + addedSkillLevelComboBoxes.IndexOf(skillLevel) + "");
-        //    }
-        //    return level;
-        //}
-
-
-        //private void btnSave_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if(dpcDateOfBirth.SelectedDate == null)
-        //    {
-        //        MessageBox.Show("Geben Sie bitte ein Geburtsdatum ein!");
-        //        return;
-        //    }
+                case "Bereits in Projekt eingesetzt":
+                    level = 3;
+                    break;
 
 
-        //    if (DatabaseConnections.EmployeeExists(tbxFirstName.Text, tbxLastName.Text, new SqlDateTime((DateTime)dpcDateOfBirth.SelectedDate)))
-        //    {
-        //        MessageBox.Show("Der Mitarbeiter existiert schon!");
-        //        return;
-        //    }
+                case "Umfangreiche Projekterfahrungen":
+                    level = 4;
+                    break;
 
 
-        //    List<string> skillNames = new List<string>();
-        //    List<int> sls = new List<int>();
-
-        //    foreach (TextBox tb in addedSkillTextBoxes)
-
-        //    {
-        //        skillNames.Add(tb.Text);
-        //    }
-
-        //    foreach (ComboBox cb in addedSkillLevelComboBoxes)
-        //    {
-        //        sls.Add(AssignSkillLevel(cb));
-        //    }
+                default: throw new ArgumentException("" + addedSkillLevelComboBoxes.IndexOf(skillLevel) + "");
+            }
+            return level;
+        }
 
 
-        //    try
-        //    {
-        //        DatabaseConnections.SaveEmployeeIntoDatabase(tbxFirstName.Text, tbxLastName.Text, new SqlDateTime((DateTime)dpcDateOfBirth.SelectedDate), tbxSkill.Text, AssignSkillLevel(cbxLevel), skillNames, sls);
-        //    }
-        //    catch (SqlException exception)
-        //    {
-        //        MessageBox.Show(exception.Message);
-        //    }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (dpcDateOfBirth.SelectedDate == null)
+            {
+                MessageBox.Show("Geben Sie bitte ein Geburtsdatum ein!");
+                return;
+            }
 
-        //    MessageBox.Show("Mitarbeiterdaten erfolgreich erfasst!");
-        //}
+
+            if (DatabaseConnections.EmployeeExists(tbxFirstName.Text, tbxLastName.Text, new SqlDateTime((DateTime)dpcDateOfBirth.SelectedDate)))
+            {
+                MessageBox.Show("Der Mitarbeiter existiert schon!");
+                return;
+            }
+
+
+            List<string> skillNames = new List<string>();
+            List<int> sls = new List<int>();
+
+            foreach (TextBox tb in addedSkillTextBoxes)
+
+            {
+                skillNames.Add(tb.Text);
+            }
+
+            foreach (ComboBox cb in addedSkillLevelComboBoxes)
+            {
+                sls.Add(AssignSkillLevel(cb));
+            }
+
+
+            try
+            {
+                DatabaseConnections.SaveEmployeeIntoDatabase(tbxFirstName.Text, tbxLastName.Text, new SqlDateTime((DateTime)dpcDateOfBirth.SelectedDate), tbxSkill.Text, AssignSkillLevel(cbxLevel), skillNames, sls);
+            }
+            catch (SqlException exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+            MessageBox.Show("Mitarbeiterdaten erfolgreich erfasst!");
+        }
     }
 }
