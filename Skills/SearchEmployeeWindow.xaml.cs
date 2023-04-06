6a2b3bbd02c8730cd6f49853319e7f0b7c7ce9b8
Laurin <l.sivabalan@outlook.de>
2023-04-06 13:07:21 +0200

    Format Ausgabe eines Employees geändert

--- Skills/SearchEmployeeWindow.xaml.cs
+++ Skills/SearchEmployeeWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using MessageBox = System.Windows.MessageBox;
 
 namespace Skills
@@ -46,6 +48,18 @@ namespace Skills
         }
 
 
+        /// <summary>
+        /// Event-Handler für den Klick auf den Suchen-Button im Fenster SearchEmployeeWindow.
+        /// Sucht nach Mitarbeitern mit dem angegebenen Vornamen, Nachnamen und Geburtsdatum in der Datenbank
+        /// und zeigt die Ergebnisse Vorname, Nachname, Skillnamen und Name des Skilllevels in der ListBox an.
+        ///Wenn ein Mitarbeiter mehrere Skills hat und in der Ergebnisliste mehr als einmal vorkommt,
+        ///wird er nur einmal mit seinem Vor- und Nachnamen angezeigt und die Liste seiner Skills wird untereinander aufgeführt.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="d"></param>
+
+
+
         private void btnSearch_Click(object sender, RoutedEventArgs d)
         {
             using (var context = new EmployeeDb())
@@ -54,6 +68,9 @@ namespace Skills
                 var lastName = tbxLastName.Text;
                 var dateOfBirth = dpcDateOfBirth.SelectedDate;
 
+                // Der Query sucht in der Datenbank nach Mitarbeitern mit dem angegebenen Vornamen, Nachnamen und Geburtsdatum.
+                // Für jeden gefundenen Mitarbeiter wird eine anonyme Typ-Instanz erstellt, die den Vor- und Nachnamen, das Geburtsdatum und eine Liste der Fähigkeiten des Mitarbeiters enthält.
+                // Die Liste der Fähigkeiten wird durch eine Unterabfrage erstellt, die nach Fähigkeiten sucht, die dem Mitarbeiter zugeordnet sind, und eine anonyme Typ-Instanz mit dem Namen der Fähigkeit und Level zurückgibt.   
                 var query = (from employee in context.Employees
                              where employee.FirstName.Contains(firstName) &&
                                    employee.LastName.Contains(lastName) &&
@@ -69,6 +86,11 @@ namespace Skills
                                          .ToList()
                              }).ToList();
 
+                // Falls der Query mindestens einen Mitarbeiter gefunden hat:
+                // Die bestehenden Einträge werden aus der ListBox gelöscht
+                // - ListBox zurück auf null     
+                // - Erstellt einen Text, der die Fähigkeiten des Mitarbeiters auflistet
+                // - Fügt den Namen des Mitarbeiters und die Fähigkeiten in die ListBox ein
                 if (query.Any())
                 {
                     lbxOutput.Items.Clear();
@@ -86,6 +108,9 @@ namespace Skills
 
                     }
                 }
+                // Falls der Query keinen Mitarbeiter gefunden hat:
+                // - Füge eine Meldung hinzu, dass keine Mitarbeiter gefunden wurden
+                // - ListBox zurück auf null
                 else
                 {
                     lbxOutput.Items.Add("Keine Mitarbeiter gefunden.");
@@ -100,7 +125,11 @@ namespace Skills
         }
 
 
-
+        /// <summary>
+        /// Gibt den Namen eines Skill-Levels als Text zurück, der auf der angegebenen Zahl basiert.
+        /// </summary>
+        /// <param name="level">Die Nummer des Skill-Levels (1-4).</param>
+        /// <returns>Der Name des Skill-Levels als Text.</returns>
         static Expression<Func<int, string>> GetSkillLevelText = (level) =>
      level == 1 ? "Grundkenntnisse" :
      level == 2 ? "Fortgeschrittene Kenntnisse" :
